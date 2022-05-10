bf7b023c6e7c59453fd7508d06008e1f1ccb466d
Vijendra <docs.vijendra@gmail.com>
2022-05-10 11:50:20 +0530

    data is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    getData()
+  },[])
+  const getData = async ()=>{
+    const hostname = `http://localhost:7000`
+    const path = `/info`
+    try {
+      const response = await fetch(`${hostname}${path}`)
+      const data = await response.json()
+      setData(data)
+    } catch (error) {
+      console.error({message : error.message})
+    }
+  }
   return (
     <div className="App">
-      
+      <table>
+        <thead>
+          <tr>
+            <th>S.N.</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((row, index) => <tr>
+              <td>{index}</td>
+              <td>
+                {/* {row.name} */}
+                <input type="text" value={row.name} style={{ outline : "none", border: "none"}}/>
+                </td>
+              <td>
+                <input type="text" value={row.age} />
+                </td>
+              <td>
+                <button>Save</button>
+              </td>
+            </tr>)  
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
